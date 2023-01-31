0c963dc2e8c582ffc46cb22bba77b10cbbe84bec
Javier Gutierrez <j.gtrez@gmail.com>
2023-01-31 22:58:40 +0000

    start sensor right away

--- read-sensor.js
+++ read-sensor.js
@@ -1,19 +1,27 @@
 "use strict";
-var sensor = require("node-dht-sensor");
+const sensor = require("node-dht-sensor");
 
-var intervalID = setInterval(() => {
-  sensor.read(22, 4, function(err, temperature, humidity) {
-    if (!err) {
-      console.log(`temp: ${temperature.toFixed(1)}°C, humidity: ${humidity.toFixed(1)}%`);
-    }
-  });
-}, 10000);
+const READ_INTERVAL = 30; //seconds
 
-function stopInterval(){
-  clearInterval(intervalID);
-  return 0;
+
+function stopInterval() {
+    clearInterval(intervalID);
+    return 0;
+}
+
+function readSensor() {
+    sensor.read(22, 4, function (err, temperature, humidity) {
+        if (!err) {
+            console.log(`temp: ${temperature.toFixed(1)}°C, humidity: ${humidity.toFixed(1)}%`);
+        }
+    });
 }
 
 //function to run when user closes using ctrl+c
-process.on('SIGINT', stopInterval); 
+process.on('SIGINT', stopInterval);
+
+
+console.log(`Reading temperature and humidity every ${READ_INTERVAL} seconds.`)
+const intervalID = setInterval(readSensor, READ_INTERVAL * 1000);
+readSensor();
 
